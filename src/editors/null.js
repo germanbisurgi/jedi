de98289683056d6f181423b85959adc9e2a71f4c
German Bisurgi <gebisurgi@protonmail.com>
2022-10-30 02:18:59 +0200

    Updated null editor

--- src/editors/null.js
+++ src/editors/null.js
@@ -1,16 +1,6 @@
 import Editor from '../editor'
-import utils from '../utils'
 
 class NullEditor extends Editor {
-  build () {
-    // label
-    const labelText = utils.getSchemaTitle(this.schema) || this.getKey()
-    const label = this.jedi.theme.getLabel(labelText, {
-      for: this.path
-    })
-    this.container.appendChild(label)
-  }
-
   sanitize (value) {
     return null
   }
