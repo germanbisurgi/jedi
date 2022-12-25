107e1334a25b1c79e36c2ed5506b077d985f48e2
German Bisurgi <gebisurgi@protonmail.com>
2022-12-25 15:17:40 +0100

    Added test for minimum validator

--- src/validators/minimum.js
+++ src/validators/minimum.js
@@ -3,13 +3,14 @@ class Minimum {
     const errors = []
 
     if (schema.typeIsNumeric() && schema.minimum()) {
-      const invalid = (value < schema.minimum())
+      const computedMinimum = schema.exclusiveMinimum() ? schema.minimum() + 1 : schema.minimum()
+      const invalid = (value < computedMinimum)
 
       if (invalid) {
         const field = schema.title() ? schema.title() : key
 
         errors.push({
-          message: field + ' must be at least ' + (schema.minimum()),
+          message: field + ' must be at least ' + computedMinimum,
           path: path
         })
       }
