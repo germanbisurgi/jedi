e9fe9dd295d91f3a4fd391be907556b6d0bf2274
German Bisurgi <gebisurgi@protonmail.com>
2023-01-04 22:46:38 +0100

    cleanup

--- src/editors/multiple.js
+++ src/editors/multiple.js
@@ -74,7 +74,6 @@ class MultipleEditor extends Editor {
 
       editor.onChange = () => {
         this.onChange()
-        this.setValue(editor.getValue(), false) // todo: set value will not work properly with this
       }
 
       this.editors.push(editor)
