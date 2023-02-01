4a70baa1f94560d95d9705e53edfcf8faf1ef300
German Bisurgi <gebisurgi@protonmail.com>
2023-02-01 01:07:36 +0100

    Improved if constrain

--- src/validation/constrains/if.js
+++ src/validation/constrains/if.js
@@ -4,7 +4,7 @@ export const _if = (validator, value, schema) => {
   let errors = []
 
   if (schema.if()) {
-    if (!schema.then() || !schema.else()) {
+    if (!schema.then() && !schema.else()) {
       return errors
     }
 
@@ -12,11 +12,21 @@ export const _if = (validator, value, schema) => {
     const ifErrors = ifEditor.validate()
 
     if (ifErrors.length === 0) {
-      const thenEditor = new Jedi({ schema: schema.then(), startValue: value })
-      errors = thenEditor.validate()
+      if (schema.then()) {
+        const thenEditor = new Jedi({ schema: schema.then(), startValue: value })
+        errors = thenEditor.validate()
+        thenEditor.destroy()
+      } else {
+        return errors
+      }
     } else {
-      const elseEditor = new Jedi({ schema: schema.else(), startValue: value })
-      errors = elseEditor.validate()
+      if (schema.else()) {
+        const elseEditor = new Jedi({ schema: schema.else(), startValue: value })
+        errors = elseEditor.validate()
+        elseEditor.destroy()
+      } else {
+        return errors
+      }
     }
   }
 
