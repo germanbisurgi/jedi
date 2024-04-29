de4f6fabe2b209ce0e23cc12a46d7e389f4abb2f
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2024-04-29 07:35:01 +0200

    Fixed path to cjs library

--- tests/unit/tests/draft2020-12.test.js
+++ tests/unit/tests/draft2020-12.test.js
@@ -1,5 +1,5 @@
 /* global describe it expect */
-const Jedi = require('../../../dist/jedi')
+const Jedi = require('../../../dist/jedi.umd.cjs')
 
 const suites = [
   require('../../../node_modules/json-schema-test-suite/tests/draft2020-12/additionalProperties.json'),
