cee0a440fc296fcc3be341ea47b7a1a2f3326f42
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2024-11-11 07:45:27 +0100

    improved allOf validator: path and loop

--- src/validation/constrains/allOf.js
+++ src/validation/constrains/allOf.js
@@ -2,18 +2,24 @@ import { isSet, removeDuplicatesFromArray } from '../../helpers/utils.js'
 import Jedi from '../../jedi.js'
 import { getSchemaAllOf } from '../../helpers/schema.js'
 
-export function allOf (validator, value, schema, key) {
+export function allOf (validator, value, schema, key, path) {
   let errors = []
   const allOf = getSchemaAllOf(schema)
 
   if (isSet(allOf)) {
     allOf.forEach((schema) => {
-      const subSchemaEditor = new Jedi({ refParser: validator.refParser, schema: schema, data: value, rootName: key })
+      const subSchemaEditor = new Jedi({ refParser: validator.refParser, schema, data: value, rootName: key })
       const subSchemaErrors = subSchemaEditor.getErrors()
       subSchemaEditor.destroy()
-      errors = [...errors, ...subSchemaErrors]
-      errors = removeDuplicatesFromArray(errors)
+
+      subSchemaErrors.forEach((error) => {
+        error.path = path
+      })
+
+      errors.push(...subSchemaErrors)
     })
+
+    errors = removeDuplicatesFromArray(errors)
   }
 
   return errors
