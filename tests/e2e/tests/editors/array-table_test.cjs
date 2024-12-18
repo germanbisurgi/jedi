e0dc9f098d5d675f251920c20b84d0a1f31bbdad
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2024-12-18 15:22:23 +0100

    Updated test scenario

--- tests/e2e/tests/editors/array-table_test.cjs
+++ tests/e2e/tests/editors/array-table_test.cjs
@@ -106,14 +106,12 @@ Scenario('@editor @array-table should @enable', ({I}) => {
   I.dontSeeElement('.jedi-ready button[disabled]:not([always-disabled])')
 })
 
-Scenario('@editor @array-table should @delete-item', async ({I}) => {
-  let numOfDeleteButtons = await I.grabNumberOfVisibleElements('.jedi-array-delete');
-  I.assertEqual(numOfDeleteButtons, 2)
+Scenario('@editor @array-table should @delete-item', ({I}) => {
+  I.seeNumberOfElements('.jedi-array-delete', 2);
   I._click('.jedi-array-delete')
   I.seeInPopup('Are you sure you want to delete this item?')
   I.acceptPopup()
-  numOfDeleteButtons = await I.grabNumberOfVisibleElements('.jedi-array-delete');
-  I.assertEqual(numOfDeleteButtons, 1)
+  I.seeNumberOfElements('.jedi-array-delete', 1);
 })
 
 Scenario('@editor @array-table should @destroy', ({I}) => {
