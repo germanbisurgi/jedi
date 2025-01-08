1e95e1ab9140b1084c0019da9d44c2eb107ac3a9
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2025-01-08 21:06:09 +0100

    Build translation into library

--- dist/esm/jedi.js
+++ dist/esm/jedi.js
@@ -3814,6 +3814,39 @@ class UiResolver {
     return null;
   }
 }
+const defaultTranslations = {
+  errorAdditionalProperties: 'Has additional property "{{ property }}" but no additional properties are allowed.',
+  errorAnyOf: "Must validate against at least one of the provided schemas.",
+  errorConst: "Must have value of: {{ const }}.",
+  errorContains: "Must contain at least one item matching the provided schema.",
+  errorDependentRequired: "Must have the required properties: {{ dependentRequired }}.",
+  errorEnum: "Must be one of the enumerated values: {{ enum }}.",
+  errorExclusiveMaximum: "Must be less than {{ exclusiveMaximum }}.",
+  errorExclusiveMinimum: "Must be greater than {{ exclusiveMinimum }}.",
+  errorFormat: "Must be a valid {{ format }}.",
+  errorItems: "Must have items that validate against the provided schema.",
+  errorMaximum: "Must be at most {{ maximum }}.",
+  errorMaxItems: "Must have at most {{ maxItems }} items.",
+  errorMaxLength: "Must be at most {{ maxLength }} characters long.",
+  errorMaxProperties: "Must have at most {{ maxProperties }} properties.",
+  errorMaxContains: "Must contain at most {{ maxContains }} items matching the provided schema. It currently contains {{ counter }}.",
+  errorMinContains: "Must contain at least {{ minContains }} items matching the provided schema. It currently contains {{ counter }}.",
+  errorMinimum: "Must be at least {{ minimum }}.",
+  errorMinItems: "Must have at least {{ minItems }} items.",
+  errorMinLength: "Must be at least {{ minLength }} characters long.",
+  errorMinProperties: "Must have at least {{ minProperties }} properties.",
+  errorMultipleOf: "Must be multiple of {{ multipleOf }}.",
+  errorNot: "Must not validate against the provided schema.",
+  errorOneOf: "Must validate against exactly one of the provided schemas. It currently validates against {{ counter }} of the schemas.",
+  errorPattern: 'Must match the pattern: "{{ pattern }}".',
+  errorPrefixItems: "Item {{ index }} fails validation.",
+  errorPropertyNames: 'Property name "{{ propertyName }}" fails validation.',
+  errorProperties: "The following properties do not comply with their schemas: {{ properties }}",
+  errorRequired: "Must have the required properties: {{ required }}.",
+  errorType: "Must be of type {{ type }}.",
+  errorUnevaluatedProperties: 'Has invalid unevaluated property "{{ property }}"',
+  errorUniqueItems: "Must have unique items."
+};
 const translations = {
   en: {
     errorAdditionalProperties: 'Has additional property "{{ property }}" but no additional properties are allowed.',
@@ -3882,44 +3915,11 @@ const translations = {
     errorUniqueItems: "Muss eindeutige Elemente haben."
   }
 };
-const defaultTranslations = {
-  errorAdditionalProperties: 'Has additional property "{{ property }}" but no additional properties are allowed.',
-  errorAnyOf: "Must validate against at least one of the provided schemas.",
-  errorConst: "Must have value of: {{ const }}.",
-  errorContains: "Must contain at least one item matching the provided schema.",
-  errorDependentRequired: "Must have the required properties: {{ dependentRequired }}.",
-  errorEnum: "Must be one of the enumerated values: {{ enum }}.",
-  errorExclusiveMaximum: "Must be less than {{ exclusiveMaximum }}.",
-  errorExclusiveMinimum: "Must be greater than {{ exclusiveMinimum }}.",
-  errorFormat: "Must be a valid {{ format }}.",
-  errorItems: "Must have items that validate against the provided schema.",
-  errorMaximum: "Must be at most {{ maximum }}.",
-  errorMaxItems: "Must have at most {{ maxItems }} items.",
-  errorMaxLength: "Must be at most {{ maxLength }} characters long.",
-  errorMaxProperties: "Must have at most {{ maxProperties }} properties.",
-  errorMaxContains: "Must contain at most {{ maxContains }} items matching the provided schema. It currently contains {{ counter }}.",
-  errorMinContains: "Must contain at least {{ minContains }} items matching the provided schema. It currently contains {{ counter }}.",
-  errorMinimum: "Must be at least {{ minimum }}.",
-  errorMinItems: "Must have at least {{ minItems }} items.",
-  errorMinLength: "Must be at least {{ minLength }} characters long.",
-  errorMinProperties: "Must have at least {{ minProperties }} properties.",
-  errorMultipleOf: "Must be multiple of {{ multipleOf }}.",
-  errorNot: "Must not validate against the provided schema.",
-  errorOneOf: "Must validate against exactly one of the provided schemas. It currently validates against {{ counter }} of the schemas.",
-  errorPattern: 'Must match the pattern: "{{ pattern }}".',
-  errorPrefixItems: "Item {{ index }} fails validation.",
-  errorPropertyNames: 'Property name "{{ propertyName }}" fails validation.',
-  errorProperties: "The following properties do not comply with their schemas: {{ properties }}",
-  errorRequired: "Must have the required properties: {{ required }}.",
-  errorType: "Must be of type {{ type }}.",
-  errorUnevaluatedProperties: 'Has invalid unevaluated property "{{ property }}"',
-  errorUniqueItems: "Must have unique items."
-};
 class Translator {
   constructor(config) {
     this.language = config.language || "en";
     this.defaultTranslations = defaultTranslations;
-    this.translations = translations;
+    this.translations = mergeDeep({}, translations, config.translations);
   }
   translate(message) {
     let translation = this.translations[this.language][message];
@@ -3966,14 +3966,15 @@ class Jedi extends EventEmitter {
       id: "",
       radiosInline: false,
       language: "en",
-      translations
+      translations: {}
     }, options);
     this.rootName = "#";
     this.pathSeparator = "/";
     this.instances = {};
     this.root = null;
     this.translator = new Translator({
-      language: this.options.language
+      language: this.options.language,
+      translations: this.options.translations
     });
     this.validator = null;
     this.schema = {};
